88f7e8f34c6ec80fb3642f9523a09c2871746764
liviaalmeidamarins <lialmeidamarins@gmail.com>
2025-06-02 19:52:21 -0300

    Adicionando foto

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,12 +1,27 @@
+import {styled} from "@mui/material"
+import Avatar from "../../../../assets/imagens/euu.jpg"
+
 const Hero = () => {
 
     const StyledHero = styled("div")(()=>({
-        
+        backgroundColor:  "black"
     }))
 
+    const StyledImg = styled("img")(()=>({
+      width: "30%",
+      borderRadius: "50%",
+        
+    }))
     return (
       <>
-       Hero 
+      
+       <StyledHero>
+        Olá
+        
+        <StyledImg src={Avatar} />
+
+       </StyledHero>
+
       </>
     )
   }
